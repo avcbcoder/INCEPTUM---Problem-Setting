d3b4c95bc6480d474a1e828307235b4ee90ca7e2
Ankit Verma <ankitvermamat@gmail.com>
2018-10-03 05:46:02 +0530

    added 'winner'

---
+++ Codechamp/winner/Solution.java
@@ -0,0 +1,17 @@
+/**
+ * @author avcbcoder
+ * last modified @03-Oct-2018 @4:49:02 AM
+ * INC Problems - Coding_Premire_League.cordinate_friends
+ * TODO
+ */
+package Codechamp.winner;
+
+public class Solution {
+	public static void main(String[] args) throws Exception {
+		System.out.println("*****       *     *     *  ");
+		System.out.println("  *        * *    **   **  ");
+		System.out.println("  *       *****   *  *  *  ");
+		System.out.println("  *      *     *  *     *  ");
+		System.out.println("*****   *       * *     *  ");
+	}
+}
